08277c8837cac8084332f4f1fafe54e37ec3d4e2
BaHerSea <glmhxc167@qq.com>
2021-09-15 10:07:43 +0800

    1

---
+++ src/One.java
@@ -0,0 +1,19 @@
+import java.util.Iterator;
+import java.util.Scanner;
+
+/**
+ * @Author 毛嘉辉
+ * @date 2021 09 15 08 04
+ */
+public class One {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String name = sc.nextLine();
+    st(name);
+  }
+
+  public static void st(String name) {
+    System.out.println("welcome to " + name);
+  }
+}
