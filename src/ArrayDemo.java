38163a092dad3a57bc5749ba00137b891b987bde
BaHerSea <glmhxc167@qq.com>
2021-09-29 08:42:38 +0800

    1

--- src/ArrayDemo.java
+++ src/ArrayDemo.java
@@ -7,33 +7,41 @@ import java.util.Scanner;
 public class ArrayDemo {
 
   public static void main(String[] args) {
+    manwei();
+  }
+
+  public static void manwei() {
     Scanner sc = new Scanner(System.in);
     String[] a = new String[]{"美国队长", "绿巨人", "蜘蛛侠", "金刚狼", "迪迦奥特曼", "黑寡妇", "蝙蝠侠", "死侍", "小丑",
         "企鹅人"};
-    // String s = sc.nextLine();
-    // if (s.equals("s")) {
-    //   for (int i = 0; i < 5; i++) {
-    //     for (int j = 0; j < 10; j++) {
-    //       System.out.println(a[j]);
-    //     }
-    //   }
-    // }
-    // String start = sc.nextLine();
-    //
-    // if (start.equals("s")) {
-    //   System.out.println("输出姓名");
-    //
-    // } else {
-    //   System.out.println("请输入s开始");
-    // }
+    String s = sc.nextLine();
+    if (s.equals("s")) {
+      for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 10; j++) {
+          System.out.println(a[j]);
+        }
+      }
+    }
+    String start = sc.nextLine();
 
+    if (start.equals("s")) {
+      System.out.println("输出姓名");
+
+    } else {
+      System.out.println("请输入s开始");
+    }
+  }
+
+  public static void xing() {
     for (int i = 0; i < 5; i++) {
       for (int j = 0; j < i + 1; j++) {
         System.out.print("*");
       }
       System.out.println();
     }
+  }
 
+  public static void Sum() {
     double sum = 0;
     for (float i = 1; i <= 100; i++) {
       if (i % 2 > 0) {
@@ -44,4 +52,5 @@ public class ArrayDemo {
     }
     System.out.println(sum);
   }
+
 }
